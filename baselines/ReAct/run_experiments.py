6da6491637d8cf68ca893f6d4302c7487a3aa931
Suleman Khan <s5solomon.khan@gmail.com>
2026-05-29 11:01:01 -0400

    Update run_experiments.py

--- baselines/ReAct/run_experiments.py
+++ baselines/ReAct/run_experiments.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from dotenv import load_dotenv
 from react_baseline import compile_react_baseline
 
-# Definition of your paper's core macro-region evaluation dataset corpus
+# Define your tasks here
 EVALUATION_TASKS = [
     {
         "id": "hospitals",
